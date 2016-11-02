e942045205e89902383e0ce82024290af4414544
popovkov57 <kevin@justjam.at>
2016-11-02 23:57:32 +0100

    Add unit test

---
+++ src/index.test.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var dikkenek = require('./index');
+
+describe('dikkenek-quotes', function(){
+  describe('all', function(){
+    it('it should be an array of string', function(){
+      expect(dikkenek.all).to.satisfy(isArrayOfString);
+
+      function isArrayOfString(array){
+        return array.every(function(item){
+          return typeof item === 'string';
+        })
+      }
+    });
+
+    it('it should contain `C’est excessivement énervant !`', function(){
+      expect(dikkenek.all).to.include('C’est excessivement énervant !');
+    });
+  });
+
+  describe('random', function(){
+    it('should return a random item from dikkenk.all', function(){
+      var randomItem = dikkenek.random();
+      expect(dikkenek.all).to.include(randomItem);
+    });
+  });
+});
